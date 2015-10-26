70b9de882b6bc695ae63f03a8e56fa802ec08bdd
Suhan Dharmasuriya <suhanr@wso2.com>
2015-10-26 10:53:52 +0530

    configuring TokenAPITestCase to run in platform and standalone modes

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/token/TokenAPITestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/token/TokenAPITestCase.java
@@ -39,6 +39,7 @@ import org.wso2.am.integration.test.utils.clients.APIStoreRestClient;
 import org.wso2.carbon.automation.engine.annotations.ExecutionEnvironment;
 import org.wso2.carbon.automation.engine.annotations.SetEnvironment;
 import org.wso2.carbon.automation.engine.context.AutomationContext;
+import org.wso2.carbon.automation.engine.context.ContextXpathConstants;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
 import org.wso2.carbon.automation.test.utils.common.FileManager;
@@ -48,6 +49,7 @@ import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
 
 import javax.ws.rs.core.Response;
+import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
@@ -56,12 +58,13 @@ import java.util.Map;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-@SetEnvironment(executionEnvironments = {ExecutionEnvironment.STANDALONE})
+@SetEnvironment(executionEnvironments = {ExecutionEnvironment.ALL})
 public class TokenAPITestCase extends APIMIntegrationBaseTest {
 
     private APIPublisherRestClient apiPublisher;
     private APIStoreRestClient apiStore;
     private ServerConfigurationManager serverConfigurationManager;
+    private String executionEnvironment;
 
     private static final Log log = LogFactory.getLog(TokenAPITestCase.class);
 
@@ -88,22 +91,27 @@ public class TokenAPITestCase extends APIMIntegrationBaseTest {
           To tests issue mentioned in https://wso2.org/jira/browse/APIMANAGER-2065 please run this test against
           WSo2 Load balancer fronted 2 gateways 2 key manager setup with WSClient mode. Please refer resource api-manager.xml file.
         */
-        String sourcePath = TestConfigurationProvider.getResourceLocation() + File.separator + "artifacts" +
-                            File.separator + "AM" + File.separator + "lifecycletest" + File.separator + "jaxrs_basic.war";
 
-        String targetPath = FrameworkPathUtil.getCarbonHome() + File.separator + "repository" + File.separator +
-                            "deployment" + File.separator + "server" + File.separator + "webapps";
+        executionEnvironment = getExecutionEnvironment();
 
-        FileManager.copyResourceToFileSystem(sourcePath, targetPath, "jaxrs_basic.war");
+        if (this.executionEnvironment.equalsIgnoreCase(ExecutionEnvironment.STANDALONE.name())) {
+            String sourcePath = TestConfigurationProvider.getResourceLocation() + File.separator + "artifacts" +
+                                File.separator + "AM" + File.separator + "lifecycletest" + File.separator + "jaxrs_basic.war";
 
-        serverConfigurationManager = new ServerConfigurationManager(
-                new AutomationContext(APIMIntegrationConstants.AM_PRODUCT_GROUP_NAME,
-                                      APIMIntegrationConstants.AM_GATEWAY_WRK_INSTANCE, TestUserMode.SUPER_TENANT_ADMIN));
+            String targetPath = FrameworkPathUtil.getCarbonHome() + File.separator + "repository" + File.separator +
+                                "deployment" + File.separator + "server" + File.separator + "webapps";
 
-        serverConfigurationManager.applyConfigurationWithoutRestart(
-                new File(getAMResourceLocation() + File.separator + "configFiles/tokenTest/" + "api-manager.xml"));
-        serverConfigurationManager.applyConfiguration(
-                new File(getAMResourceLocation() + File.separator + "configFiles/tokenTest/" + "log4j.properties"));
+            FileManager.copyResourceToFileSystem(sourcePath, targetPath, "jaxrs_basic.war");
+
+            serverConfigurationManager = new ServerConfigurationManager(
+                    new AutomationContext(APIMIntegrationConstants.AM_PRODUCT_GROUP_NAME,
+                                          APIMIntegrationConstants.AM_GATEWAY_WRK_INSTANCE, TestUserMode.SUPER_TENANT_ADMIN));
+
+            serverConfigurationManager.applyConfigurationWithoutRestart(
+                    new File(getAMResourceLocation() + File.separator + "configFiles/tokenTest/" + "api-manager.xml"));
+            serverConfigurationManager.applyConfiguration(
+                    new File(getAMResourceLocation() + File.separator + "configFiles/tokenTest/" + "log4j.properties"));
+        }
 
         apiPublisher = new APIPublisherRestClient(getPublisherURLHttp());
         apiStore = new APIStoreRestClient(getStoreURLHttp());
@@ -111,6 +119,10 @@ public class TokenAPITestCase extends APIMIntegrationBaseTest {
         apiPublisher.login(user.getUserName(), user.getPassword());
         apiStore.login(user.getUserName(), user.getPassword());
 
+    }
+
+    private String getExecutionEnvironment() throws XPathExpressionException {
+        return publisherContext.getConfigurationValue(ContextXpathConstants.EXECUTION_ENVIRONMENT);
     }
 
     @Test(groups = {"wso2.am"}, description = "Token API Test other")
@@ -288,8 +300,11 @@ public class TokenAPITestCase extends APIMIntegrationBaseTest {
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
+        executionEnvironment = getExecutionEnvironment();
         apiStore.removeApplication("TokenTestAPI-Application");
         super.cleanUp();
-        serverConfigurationManager.restoreToLastConfiguration();
+        if (this.executionEnvironment.equalsIgnoreCase(ExecutionEnvironment.STANDALONE.name())) {
+            serverConfigurationManager.restoreToLastConfiguration();
+        }
     }
 }
